ce0193194f5087c4cd9d9279efe2ae1523231909
eliserothberg <rothberg.elise@gmail.com>
2016-08-24 18:17:03 -0700

    reandom quotes populated but not randomized, types done, small fixes

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,18 +1,49 @@
 $(document).ready(function() {
 
-  var list = ['<a href="#">Humbolt County</a>', 'Mendocino', 'Sonoma', 'Napa Valley', 
+  var list = ['<a href="#">Humboldt County</a>', 'Mendocino', 'Sonoma', 'Napa Valley', 
   'Sacramento', 'San Francisco', 'Lodi', 'Livermore', 
   'Modesto', 'SantaCruz', 'Monterey/Carmel', 'Sierra Foothills', 
   'Madera', 'Fresno', 'San Luis Obispo', 'Santa Barbara', 
   'Ventura County', 'Malibu', 'Los Angeles', 'Orange County', 
   'Palm Springs', 'San Diego']
 
-  var ids = ['eureka', 'mendocino', 'sonoma', 'napa', 
+  var ids = ['humboldt', 'mendocino', 'sonoma', 'napa', 
   'sacramento', 'sanFrancisco', 'lodi', 'livermore', 
   'modesto', 'santaCruz', 'monterey', 'sierra', 
-  'madera', 'fresno', 'sanLuis', 'santaBarbara', 
+  'madera', 'fresno', 'sanLuis', 'santaBarbara',
   'ventura', 'malibu', 'losAngeles', 'orangeCounty', 
   'palmSprings', 'sanDiego']
+
+//   for (i =0; i <= ids.length; i++) {
+
+
+
+//             function findVin(){
+//                 var searchTerm="https://www.googleapis.com/customsearch/v1?key=AIzaSyCMGfdDaSfjqv5zYoS0mTJnOT3e9MURWkU&cx=005427488377789592791:p7w1yrvn9co&q=" + ids[i];
+//                 var terms=$('#location-input').val().trim();;
+//                 terms=terms.replace(/[!@#$%^&*()+=\[\]\{\}\:\;\'\",.<>?/\\|`~]/g,'');
+//                 terms=terms.replace(/ /g,'+');
+//                 searchTerm+=terms;
+//                 console.log(searchTerm);
+//                     $.ajax({url: searchTerm, method: "GET"}) 
+//                         .done(function(RETURN) {
+//                         var title=[];
+//                         console.log('got here');
+//                         for (var j=0;j<10;j++){
+//                             title[j] = {
+//                              name:RETURN.items[j].title,
+//                              url:RETURN.items[j].link
+//                             };
+//                             console.log(title[j]);
+//                             console.log(link[j]);
+//                         }
+//                             // locationRef.set(title);
+//                     });
+//                     }
+//                     return false;
+// };
+
+
 
 if (document.readyState != 'loading'){
     start();
@@ -22,210 +53,8 @@ if (document.readyState != 'loading'){
 
 function start() {
 
-//   var wineArea = $(this).attr('data-name');
-//   console.log(wineArea);
-// var queryURL = "http://services.wine.com/api/beta2/service.svc/format/catalog=" + wineArea +
-//   "1ffb6a6f186532beb31e2aad18f8f14b";
-
-//   $.ajax({
-//             url: queryURL,
-//             method: 'GET'
-//           })
-//           .done(function(response) {
-
-//               // console.log("this is id[1] " + response.data[1].id);
-//               // console.log("this is fixed_w still " + response.data[1].images.fixed_width_still.url);
-//               // console.log("this is fixed_w animated " + response.data[1].images.fixed_width.url);
-
-//             results = response.data;
-
-
-
-
-  // new Drop({
-  //   target: eureka,
-  //   position: 'top left',
-  //   openOn: 'click'
-  // });
-  // new Tether({
-  //   element: mendocino,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '190px 60px'
-  // });
-  // new Tether({
-  //   element: sierra,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '190px 210px'
-  // });
-  // new Tether({
-  //   element: sonoma,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '250px 90px'
-  // });
-  // new Tether({
-  //   element: napa,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '255px 125px'
-  // });
-  // new Tether({
-  //   element: sacramento,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '255px 180px'
-  // });
-  // new Tether({
-  //   element: sanFrancisco,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '330px 100px'
-  // });
-  // new Tether({
-  //   element: lodi,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '290px 200px'
-  // });
-  // new Tether({
-  //   element: livermore,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '330px 165px'
-  // });
-  // new Tether({
-  //   element: modesto,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'top left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '310px 210px'
-  // });
-  // new Tether({
-  //   element: santaCruz,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '20px 120px'
-  // });
-  // new Tether({
-  //   element: madera,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //    targetOffset: '0 290px'
-  // });
-  // new Tether({
-  //   element: fresno,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '60px 250px'
-  // });
-  // new Tether({
-  //   element: monterey,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '80px 170px'
-  // });
-  // new Tether({
-  //   element: sanLuis,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '135px 215px'
-  // });
-  // new Tether({
-  //   element: santaBarbara,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '185px 235px'
-  // });
-  // new Tether({
-  //   element: venturaCounty,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '200px 320px'
-  // });
-  // new Tether({
-  //   element: malibu,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '250px 340px'
-  // });
-  // new Tether({
-  //   element: losAngeles,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '200px 365px'
-  // });
-  // new Tether({
-  //   element: orangeCounty,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '255px 390px'
-  // });
-  // new Tether({
-  //   element: palmSprings,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '245px 480px'
-  // });
-  // new Tether({
-  //   element: sanDiego,
-  //   target: map,
-  //   attachment: 'bottom right',
-  //   targetAttachment: 'center left',
-  //   // offset: '20px 0px',
-  //   targetOffset: '300px 460px'
-  //   });
-// for (i = 0; i < ids.length; i++) {
-//    drop = new Drop({
-//         target: document.querySelector("#" + ids[i]),
-//         content: list[0],
-//         position: 'left middle',
-//         openOn: 'click',
-//         classes: 'drop-theme-arrows-bounce-dark'
-//     });
-// }
     drop = new Drop({
-        target: document.querySelector('#humbolt'),
+        target: document.querySelector('#humboldt'),
         content: list[0],
         position: 'left top',
         openOn: 'click',
